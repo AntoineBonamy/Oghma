75609b785d4b6f4bddf57d3fa02eb92ab53192f6
Antoine Bonamy <bonamy.antoine@hotmail.fr>
2026-02-20 16:41:25 +0100

    TS to JS. Express server works

---
+++ Backend/prisma.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+import { PrismaClient } from "@prisma/client";
+
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
+export const prisma = new PrismaClient();
+
+module.exports = prisma;
