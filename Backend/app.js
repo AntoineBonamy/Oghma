4f688aaca32ae395b9aec3e2a118fece7d013392
Antoine Bonamy <bonamy.antoine@hotmail.fr>
2026-02-20 17:10:01 +0100

    Connexion DB is OK

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { prisma } from "./prisma.js";
 
 export const app = express();
 
@@ -9,3 +10,12 @@ app.use(express.json());
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
+
+app.get("/db-test", async (_req, res) => {
+  try {
+    const userCount = await prisma.user.count();
+    res.json({users: userCount});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
--- Backend/prisma.js
+++ Backend/prisma.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL, // ta DB PostgreSQL
+});
 
-export const prisma = new PrismaClient();
-
-module.exports = prisma;
+export const prisma = new PrismaClient({
+  adapter
+});
