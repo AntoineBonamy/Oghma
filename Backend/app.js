e32511786a199cc6e1ef8435315610a1c4662430
Antoine Bonamy <bonamy.antoine@hotmail.fr>
2026-03-01 15:51:19 +0100

    Add Worlds Routes. Modified auth.middleware.js by authenticate.js. Add middleware isWorldOwner.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./src/modules/auth/auth.routes.js";
+import worldRoutes from "./src/modules/worlds/worlds.routes.js";
 
 export const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 //#===== ROUTES =====#
 
 app.use("/api/auth", authRoutes);
+app.use("/api/worlds", worldRoutes);
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
